bb463e5934f41442a7dfb209ee699516a2ba62c0
ppppp <pp.96.99.96@gmail.com>
2021-06-17 14:12:53 +0900

    リファクタ

--- html/private/common.php
+++ html/private/common.php
@@ -9,7 +9,7 @@ class Env
     public static function getValue($key)
     {
         if ((self::$dotenv instanceof Dotenv) === false) {
-            self::$dotenv = Dotenv::create(__DIR__ . './../');
+            self::$dotenv = Dotenv::create(__DIR__ . './conf/');
             self::$dotenv->load();
         }
 
