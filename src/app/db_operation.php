494b17ce92c3fb2c124c1431cdffac5434a64c63
pp <ichikawa929@yahoo.co.jp>
2022-07-12 00:13:27 +0900

    refactor: update db_operation.php

--- src/app/db_operation.php
+++ src/app/db_operation.php
@@ -1,10 +1,14 @@
 <?php
 
-$dsn = 'mysql:host=mysql;dbname=' . Env::getValue('db') . ';charset=utf8mb4';
-$db = new PDO($dsn, Env::getValue('user'), Env::getValue('password'));
-
 class dbUtill
 {
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host=mysql;dbname=' . Env::getValue('db') . ';charset=utf8mb4';
+        $this->db = new PDO($dsn, Env::getValue('user'), Env::getValue('password'));
+    }
+
     public static function insertMusicData($db, $uri, $artists, $popularity, $duration_ms, $isrc)
     {
         try {
@@ -23,25 +27,25 @@ class dbUtill
         }
     }
 
-    public static function registerUser($db, $userid)
+    public function registerUser($userid)
     {
         try {
             $sql = "INSERT INTO users (userid, used_count, register_date, update_date)
                     VALUES (?, 0, NOW(), NOW())
                     ON DUPLICATE KEY UPDATE update_date = VALUES(update_date)";
-            $stmt = $db->prepare($sql);
+            $stmt = $this->db->prepare($sql);
             $stmt->execute([$userid]);
         } catch (PDOException $e) {
             error_log($e);
         }
     }
 
-    public static function getMusic($db, $text)
+    public function getMusic($text)
     {
         try {
             $minute = substr($text, 0, 1);
             $sql = 'select * from music_data where duration_ms between (60000 * ? - 5000) and (60000 * ? + 5000) and isrc like ' . '"jp%"';
-            $stmt = $db->prepare($sql);
+            $stmt = $this->db->prepare($sql);
             $stmt->execute([$minute, $minute]);
             $result = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
@@ -53,11 +57,11 @@ class dbUtill
         }
     }
 
-    public static function updateUserCount($db, $userid)
+    public function updateUserCount($userid)
     {
         try {
             $sql = "update users set used_count = used_count + 1 where userid = ?";
-            $stmt = $db->prepare($sql);
+            $stmt = $this->db->prepare($sql);
             $stmt->execute([$userid]);
         } catch (PDOException $e) {
             error_log($e);
