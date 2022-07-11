8866981dc29c1d2a5cf37cfc7c63ca3edea328a6
pp <ichikawa929@yahoo.co.jp>
2022-07-12 00:42:40 +0900

    fix: update UseCases/User/Update.php

--- src/app/UseCases/User/Update.php
+++ src/app/UseCases/User/Update.php
@@ -1,19 +1,21 @@
 <?php
 
-include(__DIR__ . '../../db_operation.php');
+include(__DIR__ . '../../Models/User.php');
+include(__DIR__ . '../../Models/Music.php');
 
 class Update
 {
 
     public function __construct()
     {
-        $this->db = new dbUtill();
+        $this->user = new User();
+        $this->music = new Music();
     }
 
     public function invoke($event)
     {
-        $this->db->updateUserCount($event->getUserId());
-        $text = $this->db->getMusic($event->getText());
-        return $text;
+        $this->user->updateUserCount($event->getUserId());
+        $reply_text = $this->music->getMusic($event->getText());
+        return $reply_text;
     }
 }
