9e80ca31f090330f45c24698376ed248afe87170
pp <ichikawa929@yahoo.co.jp>
2022-07-28 00:41:36 +0900

    refactor: update ddl

--- initdb.d/ddl.sql
+++ initdb.d/ddl.sql
@@ -2,16 +2,15 @@ create table music(
     `id` int not null auto_increment primary key,
     `uri` VARCHAR(255), index uri_index (uri),
     `artists` VARCHAR(255),
-    `popularity` int(11),
-    `duration_ms` int(11),
+    `popularity` int(10),
+    `duration_ms` int(10),
     `isrc` VARCHAR(255),
     `register_date` datetime
     );
 
 create table users(
     `userid` VARCHAR(255) primary key,
-    `used_count` int(11),
-    `status` VARCHAR(255),
+    `used_count` int(10) default 0,
     `register_date` datetime,
     `update_date` datetime
     );
