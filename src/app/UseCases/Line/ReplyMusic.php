4927f35d04d442b90f48451a53d8b771c179f8f1
pp <ichikawa929@yahoo.co.jp>
2022-09-13 09:08:34 +0900

    refactor: update replayMusic

--- src/app/UseCases/Line/ReplyMusic.php
+++ src/app/UseCases/Line/ReplyMusic.php
@@ -12,24 +12,34 @@ class ReplyMusic
 {
     public const ONEMINUTE_TO_MSEC = 60000;
 
+    public function __construct()
+    {
+        $http_client = new CurlHTTPClient(env('LINE_CHANNEL_ACCESS_TOKEN'));
+        $this->line_bot = new LINEBot($http_client, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+    }
+
     public function invoke($event)
     {
         User::where('userid', $event->getUserId())
             ->increment('used_count');
 
         $minute = str_replace('分', '', $event->getText());
-        $reply_text = DB::table('music_data')
+        $tracks = DB::table('tracks')
             ->select('uri')
             ->where('isrc', 'like', 'jp%')
             ->whereBetween('duration_ms', [self::ONEMINUTE_TO_MSEC * $minute - 5000, self::ONEMINUTE_TO_MSEC * $minute + 5000])
-            ->get()
-            ->random();
+            ->get();
 
-        $http_client = new CurlHTTPClient(env('LINE_CHANNEL_ACCESS_TOKEN'));
-        $bot = new LINEBot($http_client, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
-        $bot->replyMessage(
-            $event->getReplyToken(),
-            new TextMessageBuilder($reply_text->uri)
-        );
+        if (count($tracks->all()) > 0) {
+            $this->line_bot->replyMessage(
+                $event->getReplyToken(),
+                new TextMessageBuilder($tracks->random()->uri)
+            );
+        } else {
+            $this->line_bot->replyMessage(
+                $event->getReplyToken(),
+                new TextMessageBuilder("該当の曲が見つかりませんでした")
+            );
+        }
     }
 }
