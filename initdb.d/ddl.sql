3d3e0f249f43098125fa91fa43d493428dcaeab8
pp <ichikawa929@yahoo.co.jp>
2022-09-19 00:30:12 +0900

    refactor: fix ddl

--- initdb.d/ddl.sql
+++ initdb.d/ddl.sql
@@ -2,15 +2,15 @@ create table tracks(
     `id` int not null auto_increment primary key,
     `uri` VARCHAR(255), index uri_index (uri),
     `artists` text,
-    `popularity` int(10),
-    `duration_ms` int(10),
+    `popularity` int,
+    `duration_ms` int,
     `isrc` VARCHAR(255),
     `register_date` datetime
     );
 
 create table users(
     `userid` VARCHAR(255) primary key,
-    `used_count` int(10) default 0,
+    `used_count` int default 0,
     `register_date` datetime,
     `update_date` datetime
     );
