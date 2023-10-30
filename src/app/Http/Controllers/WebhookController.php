14bcd1d2028cbb9e5970cdd69db29155f56105c3
pp <ichikawa929@yahoo.co.jp>
2023-10-30 18:57:12 +0900

    test

--- src/app/Http/Controllers/WebhookController.php
+++ src/app/Http/Controllers/WebhookController.php
@@ -44,8 +44,8 @@ class WebhookController extends Controller
             }
 
             // renderはcronが有料のためrequest毎にバッチを実行
-            // Artisan::call('command:getTracks');
-            // Artisan::call('command:deleteTracks');
+            Artisan::call('command:getTracks');
+            Artisan::call('command:deleteTracks');
         }
     }
 }
